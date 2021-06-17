9901c19f7fa6c568af352b002145905e831c638c
Veronica Liu <veronicaliu@Owners-Air.attlocal.net>
2021-06-17 08:08:10 -0700

    adding python fundamentals file

---
+++ Documents/ProgramAssign/fundamentals_practice.py
@@ -0,0 +1,22 @@
+print("Hello world") #prints Hello world
+
+#step 1
+name = "LeBron" #string
+age = 35 #integer
+salary = 37.44 #decimal/float
+plays_basketball = True #boolean
+jersey_number = "23" #string
+
+#step 2
+car_name = "Volkswagen"
+x = 50
+myfirst_name = "Veronica"
+
+print(type(name))
+print(type(age))
+print(type(salary))
+print(type(plays_basketball))
+print(type(jersey_number))
+print(type(car_name))
+print(type(x))
+print(type(myfirst_name))
